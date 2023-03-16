c8222ab62548be5884b17f591f855ca6a2572b98
auntenticar41jasb <auntenticar41jasb@gmail.com>
2023-03-16 16:05:07 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyA7bnOWzeKEVt1J7Q2FxwzsPG-m2FVPfIA",
+    authDomain: "autenticar41jasb2.firebaseapp.com",
+    projectId: "autenticar41jasb2",
+    storageBucket: "autenticar41jasb2.appspot.com",
+    messagingSenderId: "296941104989",
+    appId: "1:296941104989:web:8a1cbed61312ecbc5282b4",
+    measurementId: "G-L6GB9LXYTB"
+    });
